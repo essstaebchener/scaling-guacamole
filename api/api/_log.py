5e91f9c657fad8a0a9e5a25bfb4766892c60dd09
pramitdash <pramit.dash@tum.de>
2021-03-31 08:58:52 +0200

    Updated _log.py to unify LOGGER.info logging and error_list logging, added test-function to log dummy data

--- api/api/_log.py
+++ api/api/_log.py
@@ -1,52 +1,78 @@
 from typing import Dict
 from operator import itemgetter
-
-# from table_logger import TableLogger
 import tabulate
 import random
-
-ERROR_CODES = [error_code for error_code in range(50)]
-DUMMY_RESOLVED = {
-    'resolved': [{
-        'index': error_idx,
-        'code': random.choice(ERROR_CODES),
-        'text': 'Error ABC occurred, that is `resolved`'
-    } for error_idx in range(50)]
-}
+import copy
+import logging
 
 
-def get_error_counts(err_data: list) -> list:
-    """ return list of dict['code', 'count', 'text']"""
+def log_info(data: str or Dict, field='code', _get_counts=False) -> None:
+    """ Print a log of error_codes with counts (human-readable)"""
 
-    # get a list of all error codes in the list
-    err_codes = [d['code'] for d in err_data]
+    if type(data) is str:
+        LOGGER = logging.getLogger("API")
+        logging.basicConfig(level=logging.DEBUG)
+        LOGGER.info(data)
+    elif type(data) is dict:
+        # to log the generated lists, we create a deepcopy first
+        # instead of using the original dict
+        _data = copy.deepcopy(data)
+        for key, value in _data.items():
+            table_name = key  # resolved, unresolved, backlog
+            dataset = get_error_counts(value, field) if _get_counts else value
+            header = dataset[0].keys()
+            rows = [x.values() for x in dataset]
+            print("\nList : {} \n".format(table_name))
+            print("="*(7+len(table_name)))
+            print(tabulate.tabulate(rows, header, tablefmt='grid'))
+    return
+
+
+def get_error_counts(err_data: list, field='code') -> list:
+    """ return list of dict['field', 'count', 'text'] or code if no field is passed"""
+
+    # get a list of all field_values in the list
+    err_codes = [d[field] for d in err_data]
 
     for _dict in err_data:
         _dict.pop('index', None)  # delete the index key
-        _dict['count'] = err_codes.count(_dict['code'])  # add in the count of occurrences of the error_code
+        if field == 'code':
+            _dict.pop('operator_name', None)  # delete the operator_name key
+        elif field == 'operator_name':
+            _dict.pop('code', None)  # delete the code key
+            _dict.pop('text', None)  # delete the text key
+
+        _dict['count'] = err_codes.count(_dict[field])  # add in the count of occurrences of the field-values
 
     # get rid of non-unique error_codes
     # use tuples to check for uniqueness as tuples are hashable
     err_data = [dict(t) for t in {tuple(d.items()) for d in err_data}]
-    err_data = sorted(err_data, key=itemgetter('code'))  # sort the list by error code
+    err_data = sorted(err_data, key=itemgetter(field))  # sort the list by field
 
     return err_data
 
 
-def log_error_codes(data: Dict, _get_counts=False) -> str:
-    """ Print a log of error_codes with counts (human-readable)"""
-    log_text = ""
-    for key, value in data.items():
-        table_name = key  # resolved, unresolved, backlog
-        dataset = get_error_counts(value) if _get_counts else value
-        header = dataset[0].keys()
-        rows = [x.values() for x in dataset]
-        log_text += "List : {} \n".format(table_name)
-        log_text += "="*(7+len(table_name)) + "\n"
-        log_text += tabulate.tabulate(rows, header, tablefmt='grid')
-        log_text += "\r\n"
-    return log_text
+# test whether the code works
+if __name__ == '__main__':
 
+    # first test message string logging
+    log_info("dummy message #1")
 
-#if __name__ == '__main__':
-    # log_error_codes(DUMMY_RESOLVED)
+    # generate the dummy lists
+    ERROR_CODES = [error_code for error_code in range(50)]
+    OPERATOR_NAMES = ['OPERATOR A', 'OPERATOR B', 'OPERATOR C']
+    DUMMY_RESOLVED = {
+        'resolved': [{
+            'index': error_idx,
+            'code': random.choice(ERROR_CODES),
+            'text': 'Error ABC occurred, that is `resolved`',
+            'operator_name': random.choice(OPERATOR_NAMES),
+        } for error_idx in range(50)]
+    }
+
+    # log error codes by operator_name and code
+    log_info(DUMMY_RESOLVED, 'operator_name', True)
+    log_info(DUMMY_RESOLVED, 'code', True)
+
+    # test string again
+    log_info("dummy message #2")
